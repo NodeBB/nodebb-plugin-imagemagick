0dd4fd85eeb55089dc4968230331054827558647
Ben Lubar <ben.lubar@gmail.com>
2016-03-27 18:03:42 -0500

    fix #1 - unsupported mime type image/gif

--- library.js
+++ library.js
@@ -1,9 +1,19 @@
 "use strict";
 
-var os = require('os'),
-	gm = require('gm').subClass({ imageMagick: true }),
-	fs = require('fs'),
-	plugin = {};
+var os = require('os');
+var gm = require('gm').subClass({ imageMagick: true });
+var fs = require('fs');
+
+var nconf = module.parent.require('nconf');
+var path = module.parent.require('path');
+var validator = module.parent.require('validator');
+var winston = module.parent.require('winston');
+
+var meta = module.parent.require('./meta');
+var file = module.parent.require('./file');
+var plugins = module.parent.require('./plugins');
+
+var plugin = {};
 
 if (os.platform() === 'linux') {
 	require('child_process').exec('/usr/bin/which convert', function(err, stdout, stderr) {
@@ -46,5 +56,53 @@ plugin.normalise = function(data, callback) {
 		callback();
 	}
 };
+
+// mostly copied from NodeBB/src/controllers/uploads.js
+plugin.upload = function(data, callback) {
+	if (parseInt(meta.config.allowFileUploads, 10) !== 1) {
+		return callback(new Error('[[error:uploads-are-disabled]]'));
+	}
+	uploadFile(data.uid, data.image, callback);
+};
+
+function uploadFile(uid, uploadedFile, callback) {
+	if (plugins.hasListeners('filter:uploadFile')) {
+		return plugins.fireHook('filter:uploadFile', {file: uploadedFile, uid: uid}, callback);
+	}
+
+	if (!uploadedFile) {
+		return callback(new Error('[[error:invalid-file]]'));
+	}
+
+	if (uploadedFile.size > parseInt(meta.config.maximumFileSize, 10) * 1024) {
+		return callback(new Error('[[error:file-too-big, ' + meta.config.maximumFileSize + ']]'));
+	}
+
+	if (meta.config.hasOwnProperty('allowedFileExtensions')) {
+		var allowed = file.allowedExtensions();
+		var extension = path.extname(uploadedFile.name);
+		if (allowed.length > 0 && allowed.indexOf(extension) === -1) {
+			return callback(new Error('[[error:invalid-file-type, ' + allowed.join('&#44; ') + ']]'));
+		}
+	}
+
+	saveFileToLocal(uploadedFile, callback);
+}
+
+function saveFileToLocal(uploadedFile, callback) {
+	var filename = uploadedFile.name || 'upload';
+
+	filename = Date.now() + '-' + validator.escape(filename).substr(0, 255);
+	file.saveFileToLocal(filename, 'files', uploadedFile.path, function(err, upload) {
+		if (err) {
+			return callback(err);
+		}
+
+		callback(null, {
+			url: nconf.get('relative_path') + upload.url,
+			name: uploadedFile.name
+		});
+	});
+}
 
 module.exports = plugin;
