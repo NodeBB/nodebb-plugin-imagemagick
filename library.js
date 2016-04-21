b1a8b6e00c71eacab18bf76798c3335f23d75e29
Ben Lubar <ben.lubar@gmail.com>
2016-04-21 14:10:55 -0500

    simplify. requires NodeBB/NodeBB#4556

--- library.js
+++ library.js
@@ -8,6 +8,7 @@ var nconf = module.parent.require('nconf');
 var path = module.parent.require('path');
 var validator = module.parent.require('validator');
 var winston = module.parent.require('winston');
+var async = module.parent.require('async');
 
 var meta = module.parent.require('./meta');
 var file = module.parent.require('./file');
@@ -50,59 +51,15 @@ plugin.normalise = function(data, callback) {
 			if (err) {
 				return callback(err);
 			}
-			fs.writeFile(data.path, buffer, 'binary', callback);
+			fs.writeFile(data.path + '.png', buffer, 'binary', callback);
 		});
 	} else {
 		callback();
 	}
 };
 
-// mostly copied from NodeBB/src/controllers/uploads.js
-plugin.upload = function(data, callback) {
-	if (parseInt(meta.config.allowFileUploads, 10) !== 1) {
-		return callback(new Error('[[error:uploads-are-disabled]]'));
-	}
-	uploadFile(data.uid, data.image, callback);
+plugin.filetypeAllowed = function(data, callback) {
+	return callback(null);
 };
-
-function uploadFile(uid, uploadedFile, callback) {
-	if (plugins.hasListeners('filter:uploadFile')) {
-		return plugins.fireHook('filter:uploadFile', {file: uploadedFile, uid: uid}, callback);
-	}
-
-	if (!uploadedFile) {
-		return callback(new Error('[[error:invalid-file]]'));
-	}
-
-	if (uploadedFile.size > parseInt(meta.config.maximumFileSize, 10) * 1024) {
-		return callback(new Error('[[error:file-too-big, ' + meta.config.maximumFileSize + ']]'));
-	}
-
-	if (meta.config.hasOwnProperty('allowedFileExtensions')) {
-		var allowed = file.allowedExtensions();
-		var extension = path.extname(uploadedFile.name);
-		if (allowed.length > 0 && allowed.indexOf(extension) === -1) {
-			return callback(new Error('[[error:invalid-file-type, ' + allowed.join('&#44; ') + ']]'));
-		}
-	}
-
-	saveFileToLocal(uploadedFile, callback);
-}
-
-function saveFileToLocal(uploadedFile, callback) {
-	var filename = uploadedFile.name || 'upload';
-
-	filename = Date.now() + '-' + validator.escape(filename).substr(0, 255);
-	file.saveFileToLocal(filename, 'files', uploadedFile.path, function(err, upload) {
-		if (err) {
-			return callback(err);
-		}
-
-		callback(null, {
-			url: nconf.get('relative_path') + upload.url,
-			name: uploadedFile.name
-		});
-	});
-}
 
 module.exports = plugin;
